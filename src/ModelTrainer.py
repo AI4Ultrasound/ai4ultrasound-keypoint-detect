ff26c8a945ff2f2db194c7494d3505950bb9b548
AlexandreBanks6 <abanksga@unb.ca>
2026-07-06 17:05:13 -0400

    Update to model trainer class

--- src/ModelTrainer.py
+++ src/ModelTrainer.py
@@ -47,28 +47,46 @@ class ModelTrainer(nn.Module):
                     us_frames=data['images'].to(self.device,non_blocking=True) #(B, C, H, W)
                     target_keypoints=data['keypoints'].to(self.device,non_blocking=True) #(B,list[N_t,K_i,2]) where N_t=# of annotations/frame and K_i=number of keypoints per annotation
                     categories=data['categories'].to(self.device,non_blocking=True)  #(B,N_t)
+                    px_mul_x=data['px_mul_x'] #Multiplier for pixel to mm (mm=pixel*px_mul)
+                    px_mul_y=data['px_mul_y']
 
+                if self.return_mode=='clip': #Using the clip_collate_fn return where we pad the end of the time dimension with zeros
+                    us_frames=data['images'].to(self.device,non_blocking=True) #(B,T, C, H, W)
+                    padding_mask=data['padding_mask'].to(self.device,non_blocking=True) #(B,T,)
+                    target_keypoints=data['keypoints'].to(self.device,non_blocking=True) #(B,T,list[N_t,K_i,2])                    
+                    categories=data['categories'].to(self.device,non_blocking=True) #(B,T,N_t)
+                    px_mul_x=data['px_mul_x'] #Multiplier for pixel to mm (mm=pixel*px_mul)
+                    px_mul_y=data['px_mul_y']
                 #Gets the prediction:
                 pred_keypoints=self.model(us_frames)
                 #Runs the loss function
+
                 loss=self.loss_fun(pred_keypoints,target_keypoints)
-                #Backpropagation
-                self.optimizer.zero_grad()
-                loss.backward()
-                #Step the optimizer
-                self.optimizer.step()
-                #If we are using OneCycleLR scheduler
-                if self.LR_scheduler is not None:
-                    if isinstance(self.LR_scheduler,torch.optim.lr_scheduler.OneCycleLR):
-                        self.LR_scheduler.step() #Step every batch for one cycle lr
 
-                #Detach pred_keypoints and target_keypoints from computational graph
-                pred_keypoints=pred_keypoints.detach()
-                target_keypoints=target_keypoints.detach()
+                if torch.isnan(loss):
+                    self.optimizer.zero_grad()
+                    del us_frames, target_keypoints,categories,loss
+                    continue
+                else:
+                    #Backpropagation
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    #Step the optimizer
+                    self.optimizer.step()
+                    #If we are using OneCycleLR scheduler
+                    if self.LR_scheduler is not None:
+                        if isinstance(self.LR_scheduler,torch.optim.lr_scheduler.OneCycleLR):
+                            self.LR_scheduler.step() #Step every batch for one cycle lr
 
-                #Delete data to save memory
-                del us_frames, target_keypoints,categories,loss
+                    #Detach pred_keypoints and target_keypoints from computational graph
+                    pred_keypoints=pred_keypoints.detach()
+                    target_keypoints=target_keypoints.detach()
+
+                    #Delete data to save memory
+                    del us_frames, target_keypoints,categories,loss
                 
+
+
 
 
 
